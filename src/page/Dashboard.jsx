e642e963945934cdba2acb87dc6c20dfcad389cd
nub69 <shabik.ashab@gmail.com>
2024-03-16 16:34:31 +0600

    feat(page): Dashboard ui

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,8 +1,15 @@
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
 }
 
 export default Dashboard
